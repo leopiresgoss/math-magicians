12dec1fa6cff0cde962c2abaf529252fbf2e1683
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-15 13:05:48 -0600

    Improve calculator style

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,18 +9,18 @@ export default class Calculator extends React.Component {
 
   left = () => (
     <div id="left">
-      <button type="button">A/C</button>
+      <button type="button">AC</button>
       <button type="button">+/-</button>
       <button type="button">%</button>
-      <button type="button">9</button>
-      <button type="button">8</button>
       <button type="button">7</button>
-      <button type="button">6</button>
-      <button type="button">5</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
       <button type="button">4</button>
-      <button type="button">3</button>
-      <button type="button">2</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
       <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
       <button id="zero" type="button">0</button>
       <button type="button">.</button>
     </div>
