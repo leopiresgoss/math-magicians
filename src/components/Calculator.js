d7fc538f141302e5bb59dc046263f88e057514b4
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-15 11:06:35 -0600

    Change buttons order and style calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,27 +7,28 @@ export default class Calculator extends React.Component {
     </div>
   )
 
-  right = () => (
-    <div id="right">
-      <button type="button">.</button>
-      <button type="button">0</button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">%</button>
-      <button type="button">+/-</button>
+  left = () => (
+    <div id="left">
       <button type="button">A/C</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button">9</button>
+      <button type="button">8</button>
+      <button type="button">7</button>
+      <button type="button">6</button>
+      <button type="button">5</button>
+      <button type="button">4</button>
+      <button type="button">3</button>
+      <button type="button">2</button>
+      <button type="button">1</button>
+      <button id="zero" type="button">0</button>
+      <button type="button">.</button>
     </div>
   );
 
-  left = () => (
-    <div id="left">
-      <button type="button">/</button>
+  right = () => (
+    <div id="right">
+      <button type="button">÷</button>
       <button type="button">x</button>
       <button type="button">-</button>
       <button type="button">+</button>
@@ -41,8 +42,8 @@ export default class Calculator extends React.Component {
         <div className="calculator">
           <this.result />
           <div className="elem">
-            <this.right />
             <this.left />
+            <this.right />
           </div>
         </div>
       </>
