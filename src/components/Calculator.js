cbbdbde93d20b519df5cb8ecf007b6a9d81f4937
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-16 11:00:51 -0600

    Add Buttons components to make the code clear

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Result from './Result';
+import Buttons from './Buttons';
 import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
@@ -9,12 +10,15 @@ export default class Calculator extends React.Component {
       screen: '',
       calc: {},
     };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
     this.updateCalculatorScreen = this.updateCalculatorScreen.bind(this);
   }
 
-  updateCalculatorScreen = (e) => {
-    const btnContent = e.target.innerHTML;
+  handleButtonClick(btnContent) {
+    this.updateCalculatorScreen(btnContent);
+  }
 
+  updateCalculatorScreen = (btnContent) => {
     // update calculator
     this.setState((prevState) => {
       const newCalc = calculate(prevState.calc, btnContent);
@@ -37,35 +41,6 @@ export default class Calculator extends React.Component {
     });
   }
 
-  left = () => (
-    <div id="left">
-      <button type="button" onClick={this.updateCalculatorScreen}>AC</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>+/-</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>%</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>7</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>8</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>9</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>4</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>5</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>6</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>1</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>2</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>3</button>
-      <button id="zero" type="button" onClick={this.updateCalculatorScreen}>0</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>.</button>
-    </div>
-  );
-
-  right = () => (
-    <div id="right">
-      <button type="button" onClick={this.updateCalculatorScreen}>÷</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>x</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>-</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>+</button>
-      <button type="button" onClick={this.updateCalculatorScreen}>=</button>
-    </div>
-  );
-
   render() {
     const { screen } = this.state;
     return (
@@ -73,8 +48,7 @@ export default class Calculator extends React.Component {
         <div className="calculator">
           <Result result={screen} />
           <div className="elem">
-            <this.left />
-            <this.right />
+            <Buttons handleButtonClick={this.handleButtonClick} />
           </div>
         </div>
       </>
