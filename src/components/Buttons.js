05d863faa69651a34fa86e6b110554d211d30cc4
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-16 14:00:10 -0600

    Refactor Button and Result

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,48 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Buttons extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
-
-  handleButtonClick(e) {
-    const { handleButtonClick } = this.props;
+const Buttons = ({ handleButtonClick }) => {
+  const updateHandleButtonClick = (e) => {
     handleButtonClick(e.target.innerHTML);
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <div id="left">
-          <button type="button" onClick={this.handleButtonClick}>AC</button>
-          <button type="button" onClick={this.handleButtonClick}>+/-</button>
-          <button type="button" onClick={this.handleButtonClick}>%</button>
-          <button type="button" onClick={this.handleButtonClick}>7</button>
-          <button type="button" onClick={this.handleButtonClick}>8</button>
-          <button type="button" onClick={this.handleButtonClick}>9</button>
-          <button type="button" onClick={this.handleButtonClick}>4</button>
-          <button type="button" onClick={this.handleButtonClick}>5</button>
-          <button type="button" onClick={this.handleButtonClick}>6</button>
-          <button type="button" onClick={this.handleButtonClick}>1</button>
-          <button type="button" onClick={this.handleButtonClick}>2</button>
-          <button type="button" onClick={this.handleButtonClick}>3</button>
-          <button id="zero" type="button" onClick={this.handleButtonClick}>0</button>
-          <button type="button" onClick={this.handleButtonClick}>.</button>
-        </div>
-        <div id="right">
-          <button type="button" onClick={this.handleButtonClick}>÷</button>
-          <button type="button" onClick={this.handleButtonClick}>x</button>
-          <button type="button" onClick={this.handleButtonClick}>-</button>
-          <button type="button" onClick={this.handleButtonClick}>+</button>
-          <button type="button" onClick={this.handleButtonClick}>=</button>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div id="left">
+        <button type="button" onClick={updateHandleButtonClick}>AC</button>
+        <button type="button" onClick={updateHandleButtonClick}>+/-</button>
+        <button type="button" onClick={updateHandleButtonClick}>%</button>
+        <button type="button" onClick={updateHandleButtonClick}>7</button>
+        <button type="button" onClick={updateHandleButtonClick}>8</button>
+        <button type="button" onClick={updateHandleButtonClick}>9</button>
+        <button type="button" onClick={updateHandleButtonClick}>4</button>
+        <button type="button" onClick={updateHandleButtonClick}>5</button>
+        <button type="button" onClick={updateHandleButtonClick}>6</button>
+        <button type="button" onClick={updateHandleButtonClick}>1</button>
+        <button type="button" onClick={updateHandleButtonClick}>2</button>
+        <button type="button" onClick={updateHandleButtonClick}>3</button>
+        <button id="zero" type="button" onClick={updateHandleButtonClick}>0</button>
+        <button type="button" onClick={updateHandleButtonClick}>.</button>
+      </div>
+      <div id="right">
+        <button type="button" onClick={updateHandleButtonClick}>÷</button>
+        <button type="button" onClick={updateHandleButtonClick}>x</button>
+        <button type="button" onClick={updateHandleButtonClick}>-</button>
+        <button type="button" onClick={updateHandleButtonClick}>+</button>
+        <button type="button" onClick={updateHandleButtonClick}>=</button>
+      </div>
+    </>
+  );
+};
 
 Buttons.propTypes = {
   handleButtonClick: PropTypes.func.isRequired,
 };
+
+export default Buttons;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class Result extends React.Component {
-  render() {
-    const { result } = this.props;
-    return (
-      <div className="result">
-        <h3>{result}</h3>
-      </div>
-    );
-  }
-}
+const Result = ({ result }) => (
+  <div className="result">
+    <h3>{result}</h3>
+  </div>
+);
 
 Result.propTypes = {
   result: PropTypes.string.isRequired,
 };
+
+export default Result;
