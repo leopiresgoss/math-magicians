5f1bc8fcc47be1060f4fb92982abf43ef2e6a48a
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-22 13:06:16 -0600

    Add quote and style it

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Quote = () => (
   <div className="quote">
-    <h2>Welcome to our page!</h2>
+    <h2>
+      Mathematics is not about numbers, equations
+      , computations, or algorithms: it is about understanding. - William Thurston
+    </h2>
   </div>
 );
 
