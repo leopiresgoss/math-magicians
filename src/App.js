0578d76db2a046dac753dc087963cdb857f83c23
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-22 11:44:44 -0600

    Install react router and add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+// import { Routes, Route} from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 
-const App = () => <Calculator />;
+const App = () => (
+  <>
+    <Navbar />
+    <Calculator />
+  </>
+);
 
 export default App;
