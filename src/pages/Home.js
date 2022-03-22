e4f8b41ad07860e65875f2f1f6cb01a7571a5ae9
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-22 14:35:10 -0600

    Update Homepage content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,15 @@ const Home = () => (
   <div className="home">
     <h2>Welcome to our page!</h2>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident iste
-      vel doloribus debitis esse dolore, aspernatur veritatis harum quod deserunt
-      consectetur atque pariatur impedit aut illo veniam optio dolor expedita?
+      Math Magicians is a website where you can make simple calculations
+      . If you are in need of a Math magician
+      , or just a good one for learning equations in a fun way
+      , Math Magicians is the site for you!
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident iste
-      vel doloribus debitis esse dolore, aspernatur veritatis harum quod deserunt
-      consectetur atque pariatur impedit aut illo veniam optio dolor expedita?
+      Click in Calculator and use it to figure out how many apple trees are needed for 2,700
+      apples - each apple tree produce 90 apples
+      . You can find the answers to these questions and more using the calculator!
     </p>
   </div>
 );
