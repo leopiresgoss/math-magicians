455b93140e9175a105debfba923a8c351d7c61da
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-23 11:59:23 -0600

    Fix linters issues

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,7 +1,7 @@
-import renderer from "react-test-renderer";
-import Calculator from "../components/Calculator";
+import renderer from 'react-test-renderer';
+import Calculator from '../components/Calculator';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
 });
